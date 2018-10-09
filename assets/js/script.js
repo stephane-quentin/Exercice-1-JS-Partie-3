e4f645c22474f00bb3262bd05f64d7dea2898deb
Stiouf <stephane.quentin@live.fr>
2018-10-09 14:20:51 +0200

    ex1

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function bordure(){ // Une bordure de 3px rouge apparait à l'activation de cette fonction (passage de al souris sur l'image)
+  document.getElementById('image1').style.border = 'red 3px solid';
+}
+function pasbordure(){ //La bordure disparait une fois que la souris n'est plussur l'image
+  document.getElementById('image1').style.border = '0';
+}
