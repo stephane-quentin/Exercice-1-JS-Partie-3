68df6a2456c51e3b690745f00261cd6e26cc973d
Stiouf <stephane.quentin@live.fr>
2018-10-10 15:52:16 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-function bordure(){ // Une bordure de 3px rouge apparait à l'activation de cette fonction (passage de al souris sur l'image)
+function plusBorder(){ // Une bordure de 3px rouge apparait à l'activation de cette fonction (passage de al souris sur l'image)
   document.getElementById('image1').style.border = 'red 3px solid';
 }
-function pasbordure(){ //La bordure disparait une fois que la souris n'est plussur l'image
-  document.getElementById('image1').style.border = '0';
+function noBorder(){ //La bordure disparait une fois que la souris n'est plus sur l'image
+  document.getElementById('image1').style.border = null;
 }
